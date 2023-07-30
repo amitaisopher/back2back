86dce7eb718d52729409a98f7d081a87adfd5cee
Amitai Sopher <amitaisopher@gmail.com>
2023-07-30 14:20:58 +0300

    Add default error hanler so the app won't crash when error is thrown

--- src/startup/router.ts
+++ src/startup/router.ts
@@ -1,4 +1,4 @@
-import { Express, Request, Response } from 'express';
+import { Express, NextFunction, Request, Response } from 'express';
 import mongooseUsersRouter from '../controllers/mongoose/user.controller';
 import typeormProductsRouter from '../controllers/typeorm/product.controller';
 
@@ -10,5 +10,19 @@ const routerSetup = (app: Express) =>
         })
         .use('/api/mongoose/users', mongooseUsersRouter)
         .use('/api/typeorm/products', typeormProductsRouter)
+
+        // Adding default error handler - if reaching this point then the server will return an error
+        .use((error: any, req: Request, res: Response, next: NextFunction) => {
+            console.error('\x1b[31m', error);
+            if (res.headersSent) {
+                return next(error);
+            }
+            return res.status(error.status || 500).json({
+                error: {
+                    status: error.status || 500,
+                    message: error.status ? error.message : 'Internal Server Error',
+                },
+            });
+        });
 
 export default routerSetup
